1ed346d3f56ee2c79799cbe2a2c1a64528c799d9
Malcak <malcakx@gmail.com>
2021-10-30 14:56:26 -0500

    constructor

---
+++ structs/class.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type Employee struct {
+	id   int
+	name string
+}
+
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+func (e *Employee) GetId() int {
+	return e.id
+}
+
+func (e *Employee) GetName() string {
+	return e.name
+}
+
+func main() {
+	e := Employee{}
+	fmt.Printf("%v\n", e)
+	e.id = 1
+	e.name = "Name"
+	fmt.Printf("%v\n", e)
+
+	e.SetId(5)
+	fmt.Printf("%v\n", e)
+	e.SetName("Segundo")
+	fmt.Printf("%v\n", e)
+
+	fmt.Printf("%v -> %v\n", e.GetId(), e.GetName())
+}
